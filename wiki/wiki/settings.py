895413b9cfef04d7b0f614cfb469dbbd27de71ec
Rayan Tabora <superguy723@Rayans-MacBook-Pro.local>
2022-06-09 12:58:03 -0400

    Added PythonBootstrap3

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS = [
     "encyclopedia",
+    "bootstrap3",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
